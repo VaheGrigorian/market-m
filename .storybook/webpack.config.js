f89ff53f0b87ffdc039bf510213f0ff087254d04
VaheGrigorian <v_grigorian@hotmail.com>
2018-10-21 17:00:03 +0400

    storybook setup - 2

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,16 +2,44 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: [
+              "@babel/plugin-transform-runtime",
+              "@babel/plugin-proposal-object-rest-spread",
+              "react-hot-loader/babel",
+              ["react-css-modules", { webpackHotModuleReloading: true }]
+            ]
+          }
+        }
+      },
+      {
         test: /\.css$/,
         exclude: /node_modules/,
         use: [
           { loader: "style-loader" },
-          { loader: "css-loader", options: { importLoaders: 1 } },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: true,
+              localIdentName: "[path]___[name]__[local]___[hash:base64:5]",
+              importLoaders: 1
+            }
+          },
           {
             loader: "postcss-loader",
             options: {
               ident: "postcss",
-              plugins: loader => [require("precss")()]
+              plugins: loader => [
+                require("postcss-import")({ root: loader.resourcePath }),
+                require("postcss-url")(),
+                require("postcss-preset-env")()
+              ]
             }
           }
         ]
--- stories/index.js
+++ stories/index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-import Select from "../src/components/select";
+import { App } from "../src/components/app/app";
 
-storiesOf("Select", module).add("with text", () => {
-  let props = {
-    options: [{ value: "foo", text: "Foo" }]
-  };
-  return <Select onClick={action("clicked")} {...props} />;
+storiesOf("App", module).add("with text", () => {
+  return <App />;
 });
